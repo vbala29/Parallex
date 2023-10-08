bbdf4ac970bbdc536e9882c397ac292322f9e11f
Vikram Bala <vikrambala2002@gmail.com>
2023-10-07 23:41:10 -0400

    Updates

--- command/command.py
+++ command/command.py
@@ -1,19 +1,25 @@
 from concurrent import futures
+import time
 
 import grpc
 from protos.provider_command_protos import daemon_pb2
 from protos.provider_command_protos import daemon_pb2_grpc
 
+class Provider():
+    def __init__(self, ip):
+        self.ip = ip
+        self.last_seen = time.time()
 
 class CommandNode():
-    def __init__(self, ip, port):
+    def __init__(self):
         self.providers = {}
 
     def add_provider(self, provider):
         self.providers[provider.name] = provider
-        
+
 class DaemonHandler(daemon_pb2_grpc.MetricsServicer):
 
+    
     def SendStaticMetrics(self, request, context):
         print("Received static metrics")
         print(request)
@@ -26,14 +32,15 @@ class DaemonHandler(daemon_pb2_grpc.MetricsServicer):
         print(context)
         return daemon_pb2.Empty()
 
-def serve():
+def serve(cm):
     port = "50051"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    daemon_pb2_grpc.add_MetricsServicer_to_server(DaemonHandler(), server)
+    daemon_pb2_grpc.add_MetricsServicer_to_server(DaemonHandler(cm), server)
     server.add_insecure_port('[::]:' + port)
     server.start()
     print("Server started on port " + port)
     server.wait_for_termination()
 
 if __name__ == '__main__':
-    serve()
+    cm = CommandNode()
+    serve(cm)
