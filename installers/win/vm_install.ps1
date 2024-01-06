37682fc9f7f802ef519bb2be25216550a33adc0f
Anirudh Cowlagi <acowlagi@sas.upenn.edu>
2024-01-06 17:00:51 -0500

    per youtube desktop iso works but not server iso??

--- installers/win/vm_install.ps1
+++ installers/win/vm_install.ps1
@@ -1,10 +1,10 @@
 # Define VM parameters
 $vmName = "ParallexProviderVM"
 $vmMemoryMB = 2048  # Memory in megabytes
-$vmDiskSizeMB = 10960  # Disk size in megabytes
+$vmDiskSizeMB = 8960  # Disk size in megabytes
 $isoURL = "https://releases.ubuntu.com/focal/ubuntu-20.04.6-live-server-amd64.iso"  # Ubuntu ISO URL
 # Combine directory path and file name
-$vmVHDPath = "C:\Users\aniru\OneDrive\Documents\Anirudh\Senior-Spring\CIS4010\Parallex\vms"
+$vmVHDPath = "$env:USERPROFILE\VirtualBox VMs"
 $fullVHDPath = Join-Path -Path $vmVHDPath -ChildPath "$vmName.vdi"
 
 # Check if the directory for VHD path exists, if not, create it
@@ -15,7 +15,7 @@ if (!(Test-Path -Path $vmVHDPath) -or !(Test-Path -Path $fullVHDPath)) {
 # Download and install VirtualBox silently
 $virtualBoxInstallerUrl = "https://download.virtualbox.org/virtualbox/7.0.12/VirtualBox-7.0.12-159484-Win.exe"  # Update to the latest version
 $virtualBoxInstallerPath = "$env:TEMP\VirtualBox-Installer.exe"
-$isoPath = "$env:TEMP\ubuntu20.04.6-server-amd64.iso"
+$isoPath = "C:\Users\aniru\AppData\Local\Temp\ubuntu-20.04.6-live-server-amd64.iso"
 Write-Output "Downloading VirtualBox Installer..."
 # Invoke-WebRequest -Uri $virtualBoxInstallerUrl -OutFile $virtualBoxInstallerPath
 
@@ -29,7 +29,6 @@ msiexec -i VirtualBox-7.0.12-r159484.msi -passive -norestart
 Write-Output "Downloading Ubuntu ISO..."
 # Invoke-WebRequest -Uri $isoUrl -OutFile $isoPath
 
-
 Write-Output "Intializing VM..."
 # Create VM with VirtualBox
 # Check if the VM exists
@@ -41,11 +40,16 @@ if ($vmExists) {
 }
 
 & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createvm --name $vmName --ostype "Ubuntu_64" --register
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm $vmName --memory $vmMemoryMB
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createhd --filename "$fullVHDPath" --size $vmDiskSizeMB --format VDI
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createhd --filename $fullVHDPath --size $vmDiskSizeMB --format VDI
 & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl $vmName --name "SATA Controller" --add sata --controller IntelAhci
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach $vmName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$fullVHDPath"
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach $vmName --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium $isoPath
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach $vmName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $fullVHDPAth
+
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl $vmName --name "IDE Controller" --add ide
+
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm $vmName --memory $vmMemoryMB
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm $vmName --ioapic on
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm $vmName --boot1 dvd --boot2 disk --boot3 none --boot4 none
+
 
 # Set the VM's network adapter to Bridged mode
 # Retrieve all running network adapters excluding those with "Virtual"
@@ -63,3 +67,7 @@ if ($adapterName) {
 } else {
     Write-Output "No suitable network adapter found. Update within VirtualBox."
 }
+
+# Setup OS on Provider
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" unattended install $vmName --iso=$isoPath --user=parallexprovider --password=parallex --full-user-name="ParallexProvider" --time-zone=UTC
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm $vmName
