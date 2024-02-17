76f9a50f2071f56d7704fe49a6b9df40294c23be
Vikram Bala <vikrambala2002@gmail.com>
2024-02-17 03:08:14 -0500

    Things are integrating

--- ui/user_ui_backend/job_submission/job_submission_daemon.py
+++ ui/user_ui_backend/job_submission/job_submission_daemon.py
@@ -12,7 +12,7 @@ QUEUE_NAME : str = "ray_job_startup"
 _HEAD_START_DELAY_SECS : int = 5
 _EXPRESS_SERVER_IP : str = "127.0.0.1"
 
-def wait_until_status(job_id, status_to_wait_for, timeout_seconds=5):
+def wait_until_status(client, job_id, status_to_wait_for, timeout_seconds=5):
     start = time.time()
     while time.time() - start <= timeout_seconds:
         status = client.get_job_status(job_id)
@@ -23,9 +23,9 @@ def wait_until_status(job_id, status_to_wait_for, timeout_seconds=5):
 
 async def handleJobSubmissionRequest(msg):
     async with msg.process():
-        req = job_submission_request.initFromDict(json.loads(json.loads(msg.body)))
-        job_name = req["job_name"]
-        head_node_url = req["head_node_url"]
+        req = job_submission_request.initFromDict(json.loads(msg.body))
+        job_name = req.get_job_name()
+        head_node_url = req.get_head_node_url()
         print(f"Received job startup request. Params: job_name = {job_name}, head_node_url = {head_node_url}")
         print(f' Sleeping for {_HEAD_START_DELAY_SECS}')
         time.sleep(_HEAD_START_DELAY_SECS)
@@ -36,11 +36,11 @@ async def handleJobSubmissionRequest(msg):
             # Entrypoint shell command to execute
             entrypoint="python job_script.py",
             # Path to the local directory that contains the script.py file, parallex env name
-            runtime_env={"working_dir": f"../extracted/{job_name}/working_dir", "conda": "parallex_env"},
+            runtime_env={"working_dir": f"../extracted/{job_name}/", "conda": "parallex_env"},
         )
         print("Job ID = {job_id}")
 
-        wait_until_status(job_id, {JobStatus.SUCCEEDED, JobStatus.STOPPED, JobStatus.FAILED})   
+        wait_until_status(client, job_id, {JobStatus.SUCCEEDED, JobStatus.STOPPED, JobStatus.FAILED})   
         logs = client.get_job_logs(job_id)
         print(logs)
         
@@ -59,3 +59,6 @@ if __name__ == "__main__":
         aqmp.receive_messages(QUEUE_NAME, lambda msg: handleJobSubmissionRequest(msg)),
         aqmp.loop,
     )
+
+    while True:
+        pass
