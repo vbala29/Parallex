34427a64e753abe139cfcd3e4a935e70699a99b7
Andy Liu <andy.liu096@gmail.com>
2024-03-20 20:59:09 -0400

    create provider update and track job providers

--- ui/user_ui_backend/models/user.js
+++ ui/user_ui_backend/models/user.js
@@ -8,16 +8,26 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
-    email : {
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    available_pcu_count : {type: Number, required: true},
-    jobs_created: [{name: {type: String, required: true}, url: {type: String, required: true}, running: {type: Boolean, required: true},
-        creation_time: {type: Number, required: true}, termination_time: {type: Number, required: false}, 
-        cpu_count: {type: Number, required: true}, memory_count: {type: Number, required: true}, unique_id: {type: String, required: true},
-        job_cost : {type: Number, required: true}
+    available_pcu_count: { type: Number, required: true },
+    jobs_created: [{
+        name: { type: String, required: true },
+        url: { type: String, required: true },
+        running: { type: Boolean, required: true },
+        creation_time: { type: Number, required: true },
+        termination_time: { type: Number, required: false },
+        cpu_count: { type: Number, required: true },
+        memory_count: { type: Number, required: true },
+        unique_id: { type: String, required: true },
+        job_cost: { type: Number, required: true },
+        providers_assigned: [{
+            provider_id: { type: String, required: true },
+            status: { type: String, required: true } // "pending", "running", "completed"
+        }]
     }]
 
 })
--- ui/user_ui_backend/routes/providers.js
+++ ui/user_ui_backend/routes/providers.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const locals = require('../app.locals.js')
 const Provider = require(locals.models + '/provider');
 const checkAuth = require(locals.scripts + '/checkAuth');
+const User = require(locals.models + '/user');
 
 
 /* ENV variables */
@@ -35,7 +36,11 @@ router.post('/provider/login', async (req, res) => {
                     res.status(401).json({ success: false, message: "username or password incorrect" });
                 }
                 else {
-                    const token = jwt.sign({ userId: provider._id, username: provider.username }, secretKey, { expiresIn: "1h" });
+                    const token = jwt.sign({
+                        /*Required to keep as userId to match `checkAuth.js`*/
+                        userId: provider._id,
+                        username: provider.username
+                    }, secretKey, { expiresIn: "1h" });
                     res.status(200).json({ success: true, message: "Authentication successful", token: token });
                 }
             }
@@ -45,5 +50,67 @@ router.post('/provider/login', async (req, res) => {
         res.status(500).json({ error: 'Authentication failed' });
     }
 });
+
+/*
+Requires:
+job_id: Number
+job_userid: Number
+pcu_increment: Number
+time_end: Optional[Number], in Unix time
+*/
+router.post('/provider/update', checkAuth, async (req, res) => {
+
+    try {
+        const providerId = req.userData.userId;
+        const job_id = req.body.job_id;
+        const pcu_increment = req.body.pcu_increment;
+        const job_userid = req.body.job_userid;
+        const find_schema = {
+            '_id': providerId,
+            'jobs_running.user': job_user,
+            'jobs_running.id': job_id,
+        }
+
+        provider_update_command = {
+            $inc: { 'jobs_running.$.pcu_consumed': pcu_increment }
+        }
+        if (req.body.time_end) {
+            const time_end = req.body.time_end;
+            provider_update_command[$set] = { 'jobs_running.$.time_end': time_end }
+        }
+
+        await Provider.findOneAndUpdate(
+            find_schema,
+            provider_update_command,
+        )
+
+        const user_find_schema = {
+            '_id': job_userid,
+            'jobs_created.unique_id': job_id
+        }
+
+        const user = await User.findById(userId)
+        const allProvidersCompleted = user.jobs_created.every(job =>
+            job.providers_assigned.every(provider => provider.status === 'completed')
+        );
+
+        user_update_command = {
+            $inc: { 'jobs_created.$.job_cost': pcu_increment }
+        }
+
+        if (allProvidersCompleted) {
+            user_update_command[$set] = { 'jobs_created.$.running': false }
+        }
+
+        await User.findOneAndUpdate(
+            user_find_schema,
+            user_update_command
+        )
+    } catch (err) {
+        console.error("Error in Query for /provider/update: " + err);
+        res.sendStatus(500);
+    }
+    res.sendStatus(200);
+})
 
 module.exports = router;
