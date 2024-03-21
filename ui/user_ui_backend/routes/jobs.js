e0a0071160124635cca22512a9afbd95a7eecbde
Andy Liu <andy.liu096@gmail.com>
2024-03-20 23:20:59 -0400

    format jobs

--- ui/user_ui_backend/routes/jobs.js
+++ ui/user_ui_backend/routes/jobs.js
@@ -27,14 +27,14 @@ const options = {
 var packageDefinition = protoLoader.loadSync(__dirname + PROTO_PATH)
 const job = grpc.loadPackageDefinition(packageDefinition).job.Job;
 const client = new job(
-  "127.0.0.1:50051",
-  grpc.credentials.createInsecure()
+    "172.17.207.62:50051",
+    grpc.credentials.createInsecure()
 );
 
 /* Routes */
 
 router.get('/job-list', checkAuth, async (req, res) => {
-    await User.findOne({'_id' : req.userData.userId}).exec().then((async (doc) => {
+    await User.findOne({ '_id': req.userData.userId }).exec().then((async (doc) => {
         if (!doc) {
             throw "Undefined Document Error";
         }
@@ -42,7 +42,7 @@ router.get('/job-list', checkAuth, async (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(
             {
-                jobs_created : doc.jobs_created
+                jobs_created: doc.jobs_created
             }
         ));
 
@@ -50,13 +50,13 @@ router.get('/job-list', checkAuth, async (req, res) => {
         console.error("Error in Query for /job-list: " + err);
         res.sendStatus(500);
     });
-  });
+});
 
 router.put('/create-job', checkAuth, async (req, res, next) => {
     const form = formidable.formidable({ multiples: false });
     const uniqueID = uuidv4();
 
-    new Promise ((resolve, reject) => form.parse(req, async (err, fields, files) => {
+    new Promise((resolve, reject) => form.parse(req, async (err, fields, files) => {
         if (err) {
             reject("Error parsing form data")
             return;
@@ -74,31 +74,31 @@ router.put('/create-job', checkAuth, async (req, res, next) => {
         const zipFilePath = files.file[0].filepath;
 
         const extractionPath = './extracted/' + uniqueID;
-    
+
         // Create directory to extract files if it doesn't exist
         if (!fs.existsSync(extractionPath)) {
-          fs.mkdirSync(extractionPath);
+            fs.mkdirSync(extractionPath);
         }
-    
+
         // Extract the zip file
         fs.createReadStream(zipFilePath)
-          .pipe(unzipper.Extract({ path: extractionPath }))
-          .on('finish', () => {
-            resolve('Zip file extracted successfully');
-          })
-          .on('error', (err) => {
-            reject('Error extracting zip file: ' + err);
-          });
+            .pipe(unzipper.Extract({ path: extractionPath }))
+            .on('finish', () => {
+                resolve('Zip file extracted successfully');
+            })
+            .on('error', (err) => {
+                reject('Error extracting zip file: ' + err);
+            });
 
     })).then(async (msg) => {
         console.log(msg);
         console.log("lookup user with username: " + req.userData.username)
-        User.findOne({'_id' : req.userData.userId}).exec().then((doc) => {
+        User.findOne({ '_id': req.userData.userId }).exec().then((doc) => {
             if (!doc) {
                 throw "Undefined Document Error";
             } else {
                 var dummyIP = "8.8.8.8"; //Ipv4 Google DNS
-                new Promise ((resolve, reject) => client.sendJob(
+                new Promise((resolve, reject) => client.sendJob(
                     {
                         clientIP : dummyIP, 
                         cpuCount : cpu_count, 
@@ -118,17 +118,17 @@ router.put('/create-job', checkAuth, async (req, res, next) => {
                     // var head_node_url = "http://127.0.0.1:8265"
                     await doc.jobs_created.push(
                         {
-                            name : uniqueID, 
-                            url : head_node_url, 
-                            running : false,
-                            creation_time : Date.now(), 
-                            cpu_count : cpu_count,
-                            memory_count : memory_count,
-                            unique_id : uniqueID,
-                            job_cost : 0,
+                            name: uniqueID,
+                            url: head_node_url,
+                            running: false,
+                            creation_time: Date.now(),
+                            cpu_count: cpu_count,
+                            memory_count: memory_count,
+                            unique_id: uniqueID,
+                            job_cost: 0,
                         });
                     await doc.save();
-                    
+
                     aqmp.make_job_submission_request(uniqueID, head_node_url)
                     res.sendStatus(201);
                 }).catch((err) => {
@@ -141,15 +141,15 @@ router.put('/create-job', checkAuth, async (req, res, next) => {
         console.error("Error in PUT for /create-job: " + err);
         res.sendStatus(500);
     })
-    
+
 });
 
 router.get('/dashboard-info', checkAuth, async (req, res) => {
-    await User.findOne({'_id' : req.userData.userId}).exec().then(async (doc) => {
+    await User.findOne({ '_id': req.userData.userId }).exec().then(async (doc) => {
         if (!doc) {
             throw "Undefined Document Error";
         }
-        
+
         var one_month_counter = 0;
         var total_cost = 0;
         var total_job_durations = 0;
@@ -175,10 +175,10 @@ router.get('/dashboard-info', checkAuth, async (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(
             {
-                one_month_job_count : one_month_counter,
-                total_cost : total_cost,
-                avg_duration : (total_job_durations / job_list.length) / ms_in_one_minute,
-                avg_cost : total_cost / job_list.length,
+                one_month_job_count: one_month_counter,
+                total_cost: total_cost,
+                avg_duration: (total_job_durations / job_list.length) / ms_in_one_minute,
+                avg_cost: total_cost / job_list.length,
             }
         ));
     }).catch((err) => {
@@ -188,7 +188,7 @@ router.get('/dashboard-info', checkAuth, async (req, res) => {
 })
 
 router.get('/available-pcu-count', checkAuth, async (req, res) => {
-    await User.findOne({'_id' : req.userData.userId}).exec().then(async (doc) => {
+    await User.findOne({ '_id': req.userData.userId }).exec().then(async (doc) => {
         if (!doc) {
             throw "Undefined Document Error";
         }
@@ -196,7 +196,7 @@ router.get('/available-pcu-count', checkAuth, async (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(
             {
-                available_pcu_count : doc.available_pcu_count
+                available_pcu_count: doc.available_pcu_count
             }
         ));
     })
@@ -204,9 +204,9 @@ router.get('/available-pcu-count', checkAuth, async (req, res) => {
 
 router.post('buy-pcu', checkAuth, async (req, res) => {
     await User.findOneAndUpdate(
-        {'_id' : req.userData.userId}, 
-        {$inc:{ available_pcu_count: req.pcu_bought}}
-    ).exec().then( 
+        { '_id': req.userData.userId },
+        { $inc: { available_pcu_count: req.pcu_bought } }
+    ).exec().then(
         async (user) => {
             res.sendStatus(200);
         }
