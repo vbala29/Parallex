cb95de14d97e47d40cfc57a0cf949f0503795950
Vikram Bala <vikrambala2002@gmail.com>
2024-03-26 16:25:59 -0400

    Debugging file share

--- ui/user_ui_backend/routes/jobs.js
+++ ui/user_ui_backend/routes/jobs.js
@@ -40,7 +40,7 @@ router.get('/job-files', async (req, res) => {
     const job_name = req.job_name;
     const job_file_path = './job_files/' + job_name;
 
-    fs.stat(job_file_path, (req, res) => {
+    fs.stat(job_file_path, (err, stat) => {
         if (err) {
             console.error(err);
             return res.status(404).send(err);
