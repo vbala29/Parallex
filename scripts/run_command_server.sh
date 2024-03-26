95221cb415d37642d165e7f590ee14c7daf48f56
Vikram Bala <vikrambala2002@gmail.com>
2024-03-26 16:24:22 -0400

    Restart rabbit

--- scripts/run_command_server.sh
+++ scripts/run_command_server.sh
@@ -4,5 +4,6 @@ source anaconda3/etc/profile.d/conda.sh
 conda activate parallex
 cd Parallex/command
 git pull origin
+sudo systemctl restart rabbitmq-server
 nohup ./start_command_node.sh > command_node.log 2> command_node.err &
 )
