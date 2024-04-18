a87db59bb8c2f4b91674b9f7bed489ba5ed98b82
Andy <andy.liu096@gmail.com>
2024-04-18 12:32:23 -0400

    Refresh logs on redeploy for command

--- scripts/run_command_server.sh
+++ scripts/run_command_server.sh
@@ -3,6 +3,7 @@
 source anaconda3/etc/profile.d/conda.sh
 conda activate parallex
 cd Parallex/command
+rm command_node.*
 git pull origin
 sudo systemctl restart rabbitmq-server
 nohup ./start_command_node.sh > command_node.log 2> command_node.err &
